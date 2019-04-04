4aeaf27ae44db3ef7d308dda8f369e978bbc1c0c
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-04 19:25:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function holidayCountdown() {
+function holidayCountdown(days,holiday) {
   return `It's ${days} days until ${holiday}!`
 }
